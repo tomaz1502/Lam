49af0688276106a2291edab3127ef01807d47113
Tomaz Gomes <tomgm1502@gmail.com>
2022-07-27 00:57:01 -0300

    Changing AST for default DeBruijn

--- src/Lam/Expr.hs
+++ src/Lam/Expr.hs
@@ -1,14 +1,10 @@
 module Lam.Expr (Expr(..)) where
 
-type Id = String
-
 data Expr =
-    Lam Id Expr
+    Lam Expr
   | App Expr Expr
-  | Var Id
+  | Var Int
   deriving Show
 
-data Value = VLam Id Value
-
-eval :: Expr -> Value
+eval :: Expr -> Expr
 eval = undefined
