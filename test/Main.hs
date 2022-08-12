f3cfeac16a6808c244d2bdce8eb738ce9770f479
Tomaz Gomes <tomgm1502@gmail.com>
2022-08-12 16:37:12 +0300

    separate test for parser and test for eval

--- test/Main.hs
+++ test/Main.hs
@@ -7,10 +7,16 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 main :: IO ()
-main = defaultMain simpleTest
+main = defaultMain $ testGroup "lam tests" [parserTest, evalTest]
 
-simpleTest :: TestTree
-simpleTest = testCase " first test" $
+parserTest :: TestTree
+parserTest = testCase "parser test" $
   parse "(lam x -> lam y -> y . y) . (lam x -> x)"
    @?=
   App (Lam "x" (Lam "y" (App (Var 0) (Var 0)))) (Lam "x" (Var 0))
+
+evalTest :: TestTree
+evalTest = testCase "eval test" $
+  eval (App (Lam "x" (Lam "y" (App (Var 0) (Var 0)))) (Lam "x" (Var 0)))
+   @?=
+  Lam "y" (App (Var 0) (Var 0))
