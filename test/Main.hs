c2dd7d428726562bf07c13afa55123fb46a75ca3
Tomaz Gomes <tomgm1502@gmail.com>
2022-08-12 16:38:05 +0300

    refactor exampleExpr

--- test/Main.hs
+++ test/Main.hs
@@ -9,14 +9,17 @@ import Test.Tasty.HUnit
 main :: IO ()
 main = defaultMain $ testGroup "lam tests" [parserTest, evalTest]
 
+exampleExpr :: Expr
+exampleExpr = App (Lam "x" (Lam "y" (App (Var 0) (Var 0)))) (Lam "x" (Var 0))
+
 parserTest :: TestTree
 parserTest = testCase "parser test" $
   parse "(lam x -> lam y -> y . y) . (lam x -> x)"
    @?=
-  App (Lam "x" (Lam "y" (App (Var 0) (Var 0)))) (Lam "x" (Var 0))
+  exampleExpr
 
 evalTest :: TestTree
 evalTest = testCase "eval test" $
-  eval (App (Lam "x" (Lam "y" (App (Var 0) (Var 0)))) (Lam "x" (Var 0)))
+  eval exampleExpr
    @?=
   Lam "y" (App (Var 0) (Var 0))
