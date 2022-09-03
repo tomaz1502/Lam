0475e90ada846434db2fa5caa6d81c0dc77545ec
Tomaz Gomes <tomgm1502@gmail.com>
2022-09-02 21:47:10 -0300

    removing unnecessary things

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,6 @@
 module Main (main) where
 
 import Lam.Wrapper (parse)
-import Lam.Expr (eval, debugDeBruijn, Expr(..))
-
-twoFX :: Expr
-twoFX = Lam "f" (Lam "x" (App (Var 1) (App (Var 1) (Var 0))))
-
-oneFX :: Expr
-oneFX = Lam "f" (Lam "x" (App (Var 1) (Var 0)))
-
-prog2 :: String
-prog2 = "lam y -> ((lam x -> x . x) . (lam f -> f . f . y) . (lam x -> x))"
-
--- let D =  λx(x x); F= λf.(f (f y)); and I= λx.x in
---     (D  (F I))
-
-prog :: String
-prog = "(lam T -> (lam f -> lam x -> (T . f) . x)) . (lam f -> lam x -> f . f . x)"
 
 main :: IO ()
-main = do
-  print $ show $ parse "lam f -> lam x -> f . (f . x)"
-    -- print $ show $ eval $ parse prog2
-    -- print $ show $ parse prog2
+main = print $ show $ parse "lam f -> lam x -> f . (f . x)"
--- test/Fixtures/ChurchNum.hs
+++ test/Fixtures/ChurchNum.hs
@@ -4,9 +4,6 @@ import Lam.Expr ( Expr(..) )
 
 import Fixtures.Common
 
--- testCases :: [TestCase]
--- testCases = []
-
 applyN :: Int -> (a -> a) -> a -> a
 applyN 0 f a = a
 applyN n f a = f (applyN (n - 1) f a)
@@ -33,7 +30,7 @@ encodeChurchE n
 -- notice:
 -- 1. forall n, encodeChurchE n = parse (encodeChurchP n)
 -- 2. forall n and m, encodeChurchE (n * m) =
---                      eval $ App (encodeChurchE n) (encodeChurchE m)
+--                      eval $ Lam "f" (Lam "x" (App (App (encodeChurchE n) (App (encodeChurchE m) (Var 1))) (Var 0)))
 --                      (works if we swap n and m too)
 -- 3. forall n and m, encodeChurchE (n + m) =
 --   eval $ (Lambda "f" (Lambda "x" (App (encodeChurchE n) (App (Var 1) (App (encodeChurch m) (App (Var 1) (Var 0)))))))
